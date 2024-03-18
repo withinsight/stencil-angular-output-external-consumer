364cb3a14de43ff5a38122d811da714acd1772a4
Jeff Pelletier <jeffrey.pelletier@apiture.com>
2024-03-18 10:01:48 -0400

    import angular-workspace dist directory via 'npm link'

--- projects/my-consumer/src/app/app.module.ts
+++ projects/my-consumer/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ComponentLibraryModule } from 'angular-workspace/dist/component-library';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
